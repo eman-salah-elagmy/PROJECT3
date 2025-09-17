9569c2a198b2bff2dadc2877feb7011911725e6f
eman salah <158503078+engEmn@users.noreply.github.com>
2025-09-17 14:37:58 +0300

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { useSelector } from "react-redux";
 import "./App.css";
 import type { RootState } from "./apps/Store";
-import OpenedFiledBar from "./component/OpenedFiledBar";
 import Preview from "./component/Preview";
 import Recursevcompnent from "./component/Recursevcompnent";
 import ResizablePanel from "./component/ResizablePanel";
 import { filetree } from "./data/Filetree";
 import WelcomeTab from "./component/WelcomeTab";
-import DropMenue from "./component/UI/ContextMenue";
+
 
 function App() {
   const { openfile } = useSelector(({ tree }: RootState) => tree);
